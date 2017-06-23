c250cf38f27735d5e53257c4184d7278605b6166
Hannah <hannahlspencer@gmail.com>
2017-06-23 13:32:37 +0100

    Create script.js

---
+++ DrumMachineWithTimer/script.js
@@ -0,0 +1,48 @@
+var audio_context = window.AudioContext || window.webkitAudioContext;
+var con = new audio_context();
+
+
+var hat;
+
+var hat_seq = [1, 1, 0, 1, 0, 1, 0, 1];
+
+var step = 0;
+
+setInterval(function() {
+    if (hat_seq[step % hat_seq.length] == 1) {
+        playHat();
+    }
+    step = step+1;
+    
+}, 200);
+
+loadSample('cl_hat_3001.wav', function (buffer){
+    hat = buffer; 
+});
+
+
+
+function playHat(){
+     var player = con.createBufferSource();
+    player.buffer = hat;
+    player.start(); 
+    player.loop = false;
+    player.connect(con.destination); 
+}
+
+
+
+
+function loadSample(url, callback){
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+    request.onload = function(){
+        var audioData = request.response;
+        con.decodeAudioData(audioData, function(buffer) {
+            console.log(buffer);
+            callback(buffer);
+        });
+    };
+    request.send();
+}
